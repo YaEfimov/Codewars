d42000f0bc96f4b33e78afaa7e2262140c554cf1
Yaroslav <ya.efimov@internet.ru>
2023-02-06 00:00:36 +0300

    edit

--- humanReadableTime.js
+++ humanReadableTime.js
@@ -1,20 +1,22 @@
 function humanReadable (seconds) {
 
-    let hours, min;
+    if (seconds <= 359999) {
+        
+        let hours, min;
     
-    hours = Math.floor(seconds / 3600); 
+        hours = Math.floor(seconds / 3600); 
+        
+        seconds -= hours * 3600;
     
-    seconds -= hours * 3600;
-
-    min = Math.floor(seconds / 60);
-
-    seconds -= min * 60;
-
-    console.log(hours);
-    console.log(min);
-    console.log(seconds);
-
-    return '';
+        min = Math.floor(seconds / 60);
+    
+        seconds -= min * 60;
+    
+        console.log(hours);
+        console.log(min);
+        console.log(seconds);
+               
+    }
 }
 
 
